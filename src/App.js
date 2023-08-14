b4ae839fd565e81b90ae74f00d9abe5ab7d293ba
amrahmed155 <54851931+amrahmed155@users.noreply.github.com>
2023-08-14 13:47:17 +0300

    Update App.js
    changed default todo

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
-
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import styled from "styled-components";
-import ToDo from './components/ToDo';
-import ToDoListItem from './components/ToDoList';
+import ToDo from "./components/ToDo";
+import ToDoListItem from "./components/ToDoList";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 //rotas
 // import {BrowserRouter, Routes, Route} from 'react-router-dom';
 //pages
-import Home from './pages/Home';
+import Home from "./pages/Home";
 // import About from './pages/About';
 // import Contact from './pages/Contact';
 //componentes
-import Navbar from './components/Navbar';
+import Navbar from "./components/Navbar";
 // import Footer from './components/Footer/Footer';
 
 const Container = styled.div`
@@ -22,72 +21,72 @@ const Container = styled.div`
 `;
 
 function App() {
+  const [todos, setTodos] = useState([{ todo: "whats up" }]);
 
+  const AddTask = (TODO) => {
+    setTodos([...todos, { todo: TODO }]);
+    // setTodo('');
+  };
 
+  const editToDo = (editedTodo, index) => {
+    const updatedTodos = [...todos];
+    updatedTodos[index].todo = editedTodo;
+    setTodos(updatedTodos);
+  };
 
-const [todos, setTodos] = useState([{todo:'wa'}]);
+  // editItem = (index) => {
+  //   const todos = [...this.state.list];
+  //   const editedTodo = prompt('Edit the todo:');
+  //   if (editedTodo !== null && editedTodo.trim() !== '') {
+  //     let updatedTodos = [...todos]
+  //     updatedTodos[index].value= editedTodo
+  //     this.setState({
+  //       list: updatedTodos,
+  //   });
+  //   }
+  // }
 
-const AddTask = (TODO) => {
-  setTodos([...todos, { todo: TODO }]);
-  // setTodo('');
-};
+  const deleteToDo = (todoToDelete) => {
+    setTodos(todos.filter((todo) => todo !== todoToDelete));
+  };
 
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="todo"
+            element={
+              <div>
+                <Navbar />
+                <ToDo AddTask={AddTask} deleteToDo={deleteToDo} />
+                {todos.map((todo, index) => (
+                  <ToDoListItem
+                    todo={todo}
+                    index={index}
+                    editToDo={editToDo}
+                    deleteToDo={deleteToDo}
+                  />
+                ))}
+              </div>
+            }
+          />
 
-const editToDo = (editedTodo, index) => {
-  const updatedTodos = [...todos];
-  updatedTodos[index].todo = editedTodo;
-  setTodos(updatedTodos);
+          <Route
+            path=""
+            element={
+              <div>
+                <Navbar />
 
-};
-
-// editItem = (index) => {
-//   const todos = [...this.state.list];
-//   const editedTodo = prompt('Edit the todo:');
-//   if (editedTodo !== null && editedTodo.trim() !== '') {
-//     let updatedTodos = [...todos]
-//     updatedTodos[index].value= editedTodo
-//     this.setState({
-//       list: updatedTodos,
-//   });
-//   }
-// }
-
-const deleteToDo = (todoToDelete) => {
-  setTodos(todos.filter(todo => todo !== todoToDelete));
-};
-
-
-return (
-  <>
-    <BrowserRouter>
-      <Routes>
-        <Route path="todo" element={ 
-          <div>
-             <Navbar />
-          <ToDo AddTask={AddTask} deleteToDo= {deleteToDo} />
-          {todos.map((todo, index) => (
-                 
-                 <ToDoListItem  todo={todo} index ={index} editToDo={editToDo} deleteToDo={deleteToDo} />
-       
-               
-                  ))}</div>
-} />
-
- <Route path="" element ={
-  <div>
-    
-    <Navbar />
-
-        
- <Home/>
-  </div>
- }  />
-      </Routes>
-    </BrowserRouter>
+                <Home />
+              </div>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
 
       {/* <Footer /> */}
     </>
-
   );
 }
 
